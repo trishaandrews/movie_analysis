e0fec50edfda422f87d3936f3281c492a757f952
Trisha Andrews <andrews.trisha.a@gmail.com>
2015-07-09 11:32:21 -0400

    got links out of table

--- scrape_mojo.py
+++ scrape_mojo.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import re
 from bs4 import BeautifulSoup
 
 url = "http://www.boxofficemojo.com/genres/chart/?id=foreign.htm"
@@ -19,6 +20,45 @@ while code != 200:
 soup = BeautifulSoup(page)
 
 
-print soup.find('a')['href'] #href="/movies/')
+tables = soup.find_all('table')#href="/movies/')
+linklist = []
 
-print code
+for table in tables:
+    rows = table.findAll('tr')
+    for tr in rows:
+        cols = tr.findAll('td')
+        for col in cols:
+            links = col.findAll('a', href=re.compile("movies"))
+            if len(links) > 0:
+                linklist.append(links)
+    if len(linklist) > 0:
+        break
+
+links = linklist[0]
+#print tables[3].prettify()
+#for index, item in enumerate(table):
+#    if item.fetchNextSiblings('a') == []:
+#        #print index, item
+#        #item.decompose()
+
+
+#for index, item in enumerate(table):
+#    if item is not None:
+#        print index, item, type(item)
+#        linklist.append(item.find_all('a'))
+
+#print soup.find_all('a', href=re.compile("movies"))
+
+
+#something = soup.h1.find_next_siblings('table').a
+
+#find_all('a', href=re.compile("movies"))
+
+#print something
+#for t in table:
+#    print t
+#    tsoup = BeautifulSoup(t)
+#    #linklist.append(soup.find_all('a', href=re.compile("movies")))
+
+print links
+#print code
